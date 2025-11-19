b2de79a213b5b923d139213dbcc70cca578da10b
FelipeVargasdeBorba <felipevargasvb@gmail.com>
2025-11-19 00:03:51 -0300

    login e registro de contas

---
+++ login/script-login.js
@@ -0,0 +1,21 @@
+function showScreen(screen) {
+    document.getElementById("login").classList.add("hidden");
+    document.getElementById("register").classList.add("hidden");
+    document.getElementById("recover").classList.add("hidden");
+    document.getElementById(screen).classList.remove("hidden");
+}
+
+function doLogin() {
+    window.location.href = "../index.html";
+    showScreen("login");
+}
+
+function doCreate() {
+    alert("Cadastro efetuado (simulado)");
+    showScreen("login");
+}
+
+function doRecover() {
+    alert("Email de recuperação enviado (simulado)");
+    showScreen("login");
+}
